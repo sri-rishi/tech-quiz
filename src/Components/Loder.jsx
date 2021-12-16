46ac3b02c6c17be5dbbef16a6114ab11a4661ceb
Rishi Srivastava <sririshi2612@gmail.com>
2021-12-16 23:20:52 +0530

    react-cssfx-loading package added

--- src/Components/Loder.jsx
+++ src/Components/Loder.jsx
@@ -1,9 +1,12 @@
 export function Loader() {
     return (
-        <div className="loader">
-            <div className="outer"></div>
-            <div className="middle"></div>
-            <div className="inner"></div>
+        <div>
+            <h1>Loading</h1>
+            <div className="loader">
+                <div className="outer"></div>
+                <div className="middle"></div>
+                <div className="inner"></div>
+            </div>
         </div>
-    )
+       )
 }
